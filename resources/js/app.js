18e9a3b8afc1d27b674e319edc7997d69a810b0e
Damian Knut <knutdamian106@gmail.com>
2022-10-06 22:07:27 +0200

    created project

---
+++ resources/js/app.js
@@ -0,0 +1,116 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+import _ from "lodash";
+
+require('./bootstrap');
+
+
+/**
+ * Translations i18n
+ */
+
+export const isApp = (appId) => {
+    return appId === window.Laravel.appId;
+};
+
+const TRANSLATIONS = require('../lang/' + window.Laravel.locale + '.json');
+export const i18n = (t, args) => {
+    let translation = TRANSLATIONS[t] || t;
+    if (args !== undefined) {
+        _.forEach(args, (value, key) => {
+            translation = _.replace(translation, new RegExp(':' + key, 'g'), value);
+        })
+        if (args.count !== undefined) {
+            const count = args.count;
+            const splittedTranslation = translation.split('|');
+            splittedTranslation.forEach((part) => {
+                var index = part.indexOf(' ');
+                var [counter, value] = [part.slice(0, index), part.slice(index + 1)];
+                // TODO!!
+                if (counter === '{0}' && count === 0
+                  || counter === '{1}' && count === 1
+                  || counter === '[2,4]' && count >= 2 && count <= 4
+                  || counter === '[5,*]' && count >= 5
+                ) {
+                    translation = value;
+                }
+            });
+        }
+    }
+    return translation;
+};
+
+
+/**
+ * Ziggy routing in js
+ */
+
+import ziggyRoute from 'ziggy'
+import {Ziggy} from './ziggy';
+
+Ziggy.url = window.Laravel.appUrl;
+
+export const route = (name, params, absolute) => {
+    return ziggyRoute(name, params, absolute, Ziggy);
+};
+
+
+/**
+ * Date-fns library
+ */
+
+import {format, formatRelative, formatDistance, isToday, isTomorrow, parseISO} from 'date-fns';
+import {sk} from 'date-fns/locale'
+
+export const formatDate = (date, pattern) => {
+    return format(date, pattern, {locale: sk});
+};
+
+export const formatDateRel = (date) => {
+    return formatRelative(date, new Date(), {locale: sk, weekStartsOn: 1});
+};
+
+export const formatDateDist = (date) => {
+    return formatDistance(date, new Date(), {locale: sk, addSuffix: true}).replace('približne ', '');
+};
+
+
+window.Vue = require('vue').default;
+
+Vue.mixin({
+    methods: {
+        route,
+        i18n,
+        formatDate,
+        formatDateRel,
+        formatDateDist,
+        Laravel: (key) => window.Laravel[key],
+    },
+});
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+});
---
+++ webpack.mix.js
@@ -0,0 +1,49 @@
+const mix = require('laravel-mix');
+const { exec } = require('child_process');
+const path = require('path');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      ziggy: path.resolve('vendor/tightenco/ziggy/dist'),
+    },
+  },
+});
+
+mix
+  .extend('ziggy', function() {
+    if (mix.inProduction()) return;
+
+    const command = () => exec(
+      'php artisan ziggy:generate resources/shared/js/ziggy.js',
+      (error, stdout, stderr) => console.log(stdout)
+    );
+
+    command();
+
+    if (Mix.isWatching()) {
+      ((require('chokidar')).watch(['routes/**/*.php', 'config/ziggy.php']))
+        .on('change', (path) => {
+          console.log(`${path} changed...`);
+          command();
+        });
+    }
+  })
+  .options({
+    processCssUrls: false,
+  })
+  .ziggy()
+  .js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
